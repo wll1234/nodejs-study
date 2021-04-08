858f59e750acb77227fd181dd5902716df89179b
wll27471297 <wll27471297@gmail.com>
2021-04-08 22:49:14 +0900

    init WebServer

--- main.js
+++ main.js
@@ -3,5 +3,11 @@ var http = require('http');
 var app = http.createServer(function(request, response){
     var _url = request.url;
     console.log(_url);
-    
-});
+
+    if(_url == '/'){
+        _url = '/index.html';
+    }
+
+    response.writeHead(200); // OK
+    response.end(_url);
+}).listen(3000);
